26f72dbd264e290bdc5252432d7a0c279da6f418
niaz-ahsan <thatisniaz@gmail.com>
2021-03-25 04:58:59 -0700

    type qualifier const added to keep the addresses read-only

--- main.c
+++ main.c
@@ -11,8 +11,8 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-uint32_t *GPIO_D_mode = (uint32_t*) 0x40020C00; // GPIO D Mode Register Address
-uint32_t *GPIO_D_output_data = (uint32_t*) 0x40020C14; // GPIO D O/P Data Register Address
+uint32_t *const GPIO_D_mode = (uint32_t*) 0x40020C00; // GPIO D Mode Register Address
+uint32_t *const GPIO_D_output_data = (uint32_t*) 0x40020C14; // GPIO D O/P Data Register Address
 
 // Green LED
 void toggle_LD4() {
@@ -78,7 +78,7 @@ void toggle_LD6() {
 
 int main(void)
 {
-	uint32_t *RCC_clock = (uint32_t*) 0x40023830; // RCC_AHB1ENR Address
+	uint32_t *const RCC_clock = (uint32_t*) 0x40023830; // RCC_AHB1ENR Address
 
 	// setting 3rd bit of RCC Clock register to enable GPIO D peripheral
 	*RCC_clock |= (1 << 3);
